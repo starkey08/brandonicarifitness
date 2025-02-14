5dbf63716fa608f544230c321c42dbb3f5ad644e
Brandon Jones <starkey08@gmail.com>
2025-02-14 15:34:22 -0500

    Checking in progress

---
+++ src/Components/Home.tsx
@@ -0,0 +1,13 @@
+// components/Home.tsx
+import React from 'react';
+
+const Home: React.FC = () => {
+    return (
+        <div>
+            <h1>Welcome to the Home Page</h1>
+            {/* Add content for the home page */}
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/Components/Navbar.tsx
@@ -0,0 +1,18 @@
+// components/Navbar.tsx
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Navbar: React.FC = () => {
+    return (
+        <nav>
+            <ul>
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                {/* Add more navigation links as needed */}
+            </ul>
+        </nav>
+    );
+};
+
+export default Navbar;
